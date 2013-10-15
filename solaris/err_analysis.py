1af4ae22e5f1c43b8d3af6957785a8cc3635ee71
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2013-10-15 14:27:05 +0300

    run hk15

--- solaris/err_analysis.py
+++ solaris/err_analysis.py
@@ -102,7 +102,4 @@ def err_analysis(y_pred, y_test=None, X_test=None, station_info=None, date=None,
         station_residuals.iloc[:, stid].plot(ax=ax)
         ax.set_title('Station id %d' % stid)
 
-    import IPython
-    IPython.embed()
-
     plt.show()
